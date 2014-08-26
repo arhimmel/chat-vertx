6055e49cfda60f03721290757d09362d41fffc40
Ariel Himmelstern <arhimmel@gmail.com>
2014-08-26 12:43:20 -0500

    js stuff

--- src/main/java/com/arhimmel/main.java
+++ src/main/java/com/arhimmel/main.java
@@ -1,5 +1,7 @@
 package com.arhimmel;
 
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
@@ -7,12 +9,19 @@ public class Main extends Verticle {
 
     @Override
     public void start() {
-
         final Logger logger = container.logger();
 
         logger.info("Starting up");
 
         container.deployModule("io.vertx~mod-web-server~2.0.0-final", container.config());
 
+        vertx.setPeriodic(1000, new Handler<Long>() {
+            @Override
+            public void handle(Long event) {
+                JsonObject json = new JsonObject();
+                json.putString("test", "blah");
+                vertx.eventBus().publish("chat.messages", json);
+            }
+        });
     }
 }
